2b6a34c36cf5638763b72c45235c90fb6edb4d65
salakhov <salakhov@inbox.ru>
2019-10-14 13:29:08 +0300

    1. Вынес логику по взаимодействию с пользователями в класс userservice 2. Выделил отдельную сущность для пользователя

--- teststudent/task01/src/main/java/ru/otus/task01/service/UserServiceImpl.java
+++ teststudent/task01/src/main/java/ru/otus/task01/service/UserServiceImpl.java
@@ -1,19 +1,50 @@
 package ru.otus.task01.service;
 
+import ru.otus.task01.domain.Answer;
+import ru.otus.task01.domain.Puzzle;
+import ru.otus.task01.domain.User;
+
+import java.util.Scanner;
+
 /**
  * Реализация сервиса для взаимодействия с пользователем.
  */
 
 public class UserServiceImpl implements UserService {
     private TestService testService;
+    private Scanner scanner;
+    private User user;
 
     public UserServiceImpl(TestService testService) {
         this.testService = testService;
+        this.scanner = new Scanner(System.in);
+        user = new User();
     }
 
     @Override
     public void runTest(){
         testService.read();
-        testService.startInteractiveTest();
+        readUserProperties();
+
+        for(Puzzle pzl:testService.getPuzzleList()){
+            System.out.println(pzl.getQuestion().toString());
+            System.out.println("Варианты ответов:");
+            for(Answer ans:pzl.getAnswerList()){
+                System.out.println(ans.getAnswer().toString());
+            }
+            System.out.println("Введите ответ без учета регистра: ");
+            Answer ans = new Answer(scanner.nextLine());
+            pzl.setUserAnswer(ans);
+        }
+        testService.printResult();
     }
+
+    public void readUserProperties(){
+        System.out.println("Имя:");
+        user.setFirstName(scanner.nextLine());
+        System.out.println("Фамилия:");
+        user.setSecondName(scanner.nextLine());
+    }
+
+
 }
