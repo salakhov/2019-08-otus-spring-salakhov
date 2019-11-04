c413092432f26d01073bebba931dc4111c1c5228
salakhov <salakhov@inbox.ru>
2019-11-04 19:14:40 +0300

    удалил ненужные комментарии

--- teststudent/task04/src/main/java/ru/otus/task04/Main.java
+++ teststudent/task04/src/main/java/ru/otus/task04/Main.java
@@ -2,17 +2,11 @@ package ru.otus.task04;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import ru.otus.task04.service.TestService;
 
-
-//@ComponentScan("ru.otus.task04")
 @SpringBootApplication
 public class Main{
 
     public static void main(String[] args) {
 	    ApplicationContext appContext = SpringApplication.run(Main.class, args);
-        //TestService testService = appContext.getBean(TestService.class);
-        //
 	}
 }
