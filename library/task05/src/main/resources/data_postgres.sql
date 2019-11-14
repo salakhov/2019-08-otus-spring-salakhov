a818817e72ff03f29298ecbe67ece77a1ba5d689
Михаил Салахов <salahov@primetver.ru>
2019-11-14 17:44:09 +0300

    исправил ошибки

--- library/task05/src/main/resources/data_postgres.sql
+++ library/task05/src/main/resources/data_postgres.sql
@@ -1,12 +1,12 @@
 -- insert into BOOKS (id, `TITLE`) values (1, 'AAAA');
-INSERT INTO AUTHORS (ID,FIRST_NAME, SECOND_NAME,LAST_NAME) VALUES (1, 'Александр','Сергеевич','Пушкин');
-INSERT INTO AUTHORS (ID,FIRST_NAME, SECOND_NAME,LAST_NAME) VALUES (2, 'Михаил','Афанасьевич','Булгаков');
+INSERT INTO authors (ID,FIRST_NAME, SECOND_NAME,LAST_NAME) VALUES (1, 'Александр','Сергеевич','Пушкин');
+INSERT INTO authors (ID,FIRST_NAME, SECOND_NAME,LAST_NAME) VALUES (2, 'Михаил','Афанасьевич','Булгаков');
 
-INSERT INTO BOOKS (ID,TITLE) VALUES (1, 'Евгений Онегин');
-INSERT INTO BOOKS (ID,TITLE) VALUES (2, 'Местер и маргарита');
+INSERT INTO books (ID,TITLE) VALUES (1, 'Евгений Онегин');
+INSERT INTO books (ID,TITLE) VALUES (2, 'Местер и маргарита');
 
-INSERT INTO GENRES (ID,NAME) VALUES (1, 'классика');
-INSERT INTO GENRES (ID,NAME) VALUES (2, 'драма');
+INSERT INTO genres (ID,NAME) VALUES (1, 'классика');
+INSERT INTO genres (ID,NAME) VALUES (2, 'драма');
 
 select * from books;
 
