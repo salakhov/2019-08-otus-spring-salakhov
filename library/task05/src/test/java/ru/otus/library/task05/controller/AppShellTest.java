70fb025f56870e47d21aced068065e4b091088c1
Михаил Салахов <salahov@primetver.ru>
2019-11-15 11:08:35 +0300

    каркас для тестов

---
+++ library/task05/src/test/java/ru/otus/library/task05/controller/AppShellTest.java
@@ -0,0 +1,43 @@
+package ru.otus.library.task05.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.shell.Shell;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppShellTest {
+    private Shell shell;
+
+    private static final String GREETING_PATTERN="Welcome: %s";
+    private static final String DEFAILT_LOGIN = "user";
+    private static final String CUSTOM_LOGIN = "alex";
+
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void getAuthor() {
+    }
+
+    @Test
+    void getAllAuthor() {
+    }
+
+    @Test
+    void getAllAuthorByLastname() {
+    }
+
+    @Test
+    void getAllBooks() {
+    }
+
+    @Test
+    void getAllGenres() {
+    }
+
+    @Test
+    void searchAllAuthorsOfBook() {
+    }
+}
---
+++ library/task05/src/test/java/ru/otus/library/task05/dao/AuthorDaoImplTest.java
@@ -0,0 +1,20 @@
+package ru.otus.library.task05.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorDaoImplTest {
+
+    @Test
+    void getAuthorById() {
+    }
+
+    @Test
+    void getAuthorByLastName() {
+    }
+
+    @Test
+    void getAllAuthors() {
+    }
+}
